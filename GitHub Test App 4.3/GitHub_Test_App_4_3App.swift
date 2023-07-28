c0d57f80281d4978d3dac6477641e16bfdea3c4b
Chelsea Poppleton <chelseap@new.rr.com>
2023-07-28 12:23:36 -0500

    Initial Commit

---
+++ GitHub Test App 4.3/GitHub_Test_App_4_3App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_Test_App_4_3App.swift
+//  GitHub Test App 4.3
+//
+//  Created by Chelsea Poppleton on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_Test_App_4_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
